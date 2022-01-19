74087ac3250e2b300c83a25ed41b8034b8e9f924
DarkSkyFullOFStars <34119991+darkskyfullofstars@users.noreply.github.com>
2022-01-19 23:27:43 +0000

    Update auto-allservices.ps1

--- auto-allservices.ps1
+++ auto-allservices.ps1
@@ -1,49 +1,7 @@
-$services = @(
-    # Services Considered To Have Spying Capabilities
-    "DcpSvc"                                   # Data Collection and Publishing Service
-    "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
-    "DiagTrack"                                # Diagnostics Tracking Service
-    "SensrSvc"                                 # Monitors Various Sensors
-    "dmwappushservice"                         # WAP Push Message Routing Service
-    "lfsvc"                                    # Geolocation Service
-    "MapsBroker"                               # Downloaded Maps Manager
-    "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
-    "RemoteAccess"                             # Routing and Remote Access
-    "RemoteRegistry"                           # Remote Registry
-    "SharedAccess"                             # Internet Connection Sharing (ICS)
-    "TrkWks"                                   # Distributed Link Tracking Client
-    "WbioSrvc"                                 # Windows Biometric Service
-    "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
-    "WSearch"                                  # Windows Search
-    # Gaming Based Services
-    "XblAuthManager"                           # Xbox Live Auth Manager
-    "XblGameSave"                              # Xbox Live Game Save Service
-    "XboxNetApiSvc"                            # Xbox Live Networking Service
-    # Windows HomeGroup Services
-    "HomeGroupListener"                        # HomeGroup Listener
-    "HomeGroupProvider"                        # HomeGroup Provider
-    # Other Optional
-    #"bthserv"                                 # Bluetooth Support Service
-    #"wscsvc"                                  # Security Center Service
-    #"WlanSvc"                                 # WLAN AutoConfig
-    "OneSyncSvc"                               # Sync Host Service
-    "AeLookupSvc"                              # Application Experience Service
-    "PcaSvc"                                   # Program Compatibility Assistant
-    #"WinHttpAutoProxySvc"                     # WinHTTP Web Proxy Auto-Discovery
-    "UPNPHOST"                                 # Universal Plug & Play Host
-    "ERSVC"                                    # Error Reporting Service
-    "WERSVC"                                   # Windows Error Reporting Service
-    "SSDPSRV"                                  # SSDP Discovery Service
-    "CDPSvc"                                   # Connected Devices Platform Service
-    "DsSvc"                                    # Data Sharing Service
-    "DcpSvc"                                   # Data Collection and Publishing Service
-    "lfsvc"                                    # Geolocation service
-)
+# Starting and Setting to Automatic: Windows Event Collector
+Function Wecsvc{
+	Write-Output"Starting and Setting to Automatic: Wecsvc"
+	Start-Service"Wecsvc"-WarningActionSilentlyContinue
+	Set-Service"Wecsvc"-StartupTypeAutomatic
 
-foreach ($service in $services) {
-    if ( Get-Service "$service*" -Include $service ) {
-        Write-Host " Disabling Service $service ..."
-        Get-Service -Name $service | Stop-Service -Force
-        Get-Service -Name $service | Set-Service -StartupType Disabled
-    }
 }
