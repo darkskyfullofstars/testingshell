e73cc91787412b9974aa5d1baffcfcc4a873c84f
DarkSkyFullOFStars <34119991+darkskyfullofstars@users.noreply.github.com>
2022-01-19 23:44:34 +0000

    Create auto-allservices.ps1

--- auto-allservices.ps1
+++ auto-allservices.ps1
@@ -23,6 +23,26 @@ $services = @(
 )
 
 
+# Holder for None (Must keep)
+Function None {
+}
+
+# Normalize path to preset file
+$preset = ""
+$PSCommandArgs = $args
+If ($args -And $args[0].ToLower() -eq "-preset") {
+    $preset = Resolve-Path $($args | Select-Object -Skip 1)
+    $PSCommandArgs = "-preset `"$preset`""
+}
+
+# Load function names from command line arguments or a preset file
+If ($args) {
+    $tweaks = $args
+    If ($preset) {
+        $tweaks = Get-Content $preset -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
+    }
+}
+
 # Call the desired tweak functions
 $tweaks | ForEach { Invoke-Expression $_ }
 Read-Host -Prompt "Press Enter to exit"
